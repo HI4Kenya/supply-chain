4a4e535c3ffe1e980edb0edc8bbc7c7b567d282f
kwahome <kevowahome@gmail.com>
2015-07-31 09:52:25 +0300

    Updates

--- client/templates/userguide.php
+++ client/templates/userguide.php
@@ -3,59 +3,90 @@
     if($_SESSION["user_role"]=="ADMIN")
     {
         echo "<div class = 'col-md-3' style = 'font-family:arial'>
-                <span style = 'color:blue;margin-bottom:5px'>Create Users</span>
+                <span style = 'color:blue;margin-bottom:5px'>Users</span>
                 <p class = 'lead text-justify' style = 'font-size:10pt'>
-                    Create users and assign them roles. Users can be created by entering their details
-                    and login credentials or by pulling existing <a href = '".$dhis_url."' target = '_blank'> 
-                    DHIS2 </a> users.
-                    
+                    Use this Menu for creating users and assigning them roles. Users can be created by entering 
+                    their details and login credentials or by pulling existing <a href = '".$dhis_url."' target = '_blank'> 
+                    DHIS2 </a> users.                    
                     Go to 
                     <a data-toggle='collapse' data-parent='#accordion' href='#collapseUsers'>
                     USERS
-                    </a>, click on the create to enter user details or create from DHIS to pull existing users
-                    from DHIS2.
-                    You can also Edit user details and Activate/Deactivate users.
+                    </a>, 
+                    click on CREATE button to enter the details for a new user or click on CREATE FROM DHIS button
+                    to pull existing users from DHIS2. 
+                    You can also edit user details and roles by clicking on the UPDATE button. Only Admin level users 
+                    can activate/deactivate other users.
                 </p>
             </div>";
 
         echo "<div class = 'col-md-3' style = 'font-family:arial'>
-                <span style = 'color:blue;margin-bottom:5px'>Programs and Classifying</span>
+                <span style = 'color:blue;margin-bottom:5px'>Programs</span>
                 <p class = 'lead text-justify' style = 'font-size:10pt'>
-                    Create programs that will be used in determination of the supply chain hierarchy.
-                    Programs have their own distinct classification of sites.
-                    Use <a data-toggle='collapse' data-parent='#accordion' href = '#collapsePrograms'>PROGRAMS</a>
-                    to create and edit programs then go to 
+                    Use this menu to create new programs for which you want to map the supply chain hierarchy, e.g.
+                    Family Planning, ART. NB: different Programs classify their types of sites differently. 
+                    Go to <a data-toggle='collapse' data-parent='#accordion' href = '#collapsePrograms'>PROGRAMS</a>,
+                    then click on CREATE to create a new program. Click on UPDATE to edit the
+                    details of an existing program. Only Admin level users can delete programs.
+                </p>
+            </div>";
+
+        echo "<div class = 'col-md-3' style = 'font-family:arial'>
+                <span style = 'color:blue;margin-bottom:5px'>Classify</span>
+                <p class = 'lead text-justify' style = 'font-size:10pt'>
+                    This menu allows the user to create facilities by the types relevant to the supply chain hierarchy of a
+                    program. 4 types have been catered for: Sub-county stores, Central sites, Standalone sites and 
+                    Satellite sites (also caters for the Central site Dispensing point). Go to 
                     <a data-toggle='collapse' data-parent='#accordion' href='#collapseFacilities'>
-                    CLASSIFY FACILITIES
-                    </a>, to make classifications under that program. Follow the prompts
+                    CLASSIFY
+                    </a>
+                    , then click on 
+                    the relevant type of facility, e.g. Sub-county stores, Central sites, Standalone sites and Satellite
+                    sites. Select the relevant program then add facilities under the selected facility type. The list of 
+                    facilities is synchronised with the master facility list in the <a href = '".$dhis_url."' target = '_blank'> DHIS2 </a>. 
+                    Use the <span style = 'color:green'>ORGANIZATION UNITS </span>hierarchy on the side of the Classify
+                    page to narrow down to the required county, sub-county and hen facility. 
+                    Click on the facility name to select it.
                 </p>
             </div>";
 
         echo "<div class = 'col-md-3' style = 'font-family:arial'>
-                <span style = 'color:blue;'>Sorting using Org. Units</span>
-                <p class = 'lead text-justify' style = 'font-size:10pt'>
-                    There are over 9,474 facilities in the Kenya captured in 
-                    <a href = '".$dhis_url."' target = '_blank'> DHIS2 </a>.
-                    It is impossible to consider all of them at once for classification.
-                    Use the <span style = 'color:green'>ORGANIZATION UNITS</span>
-                    hierarchy on the right of the classification page
-                    to narrow down to low level units.
-                    Click on an organization unit to get a list of facilities under it.
-                </p>
-            </div>";
+                    <span style = 'color:blue;'>Reports</span>
+                    <p class = 'lead text-justify' style = 'font-size:10pt'>
+                        This menu allows the user to access various reports generated using the available data.
+                        Go to <span style = 'color:green'> DATA SET REPORT </span>to access the report for a 
+                        particular program, data set (reporting tool), reporting period and facility. Select the
+                        facility by drilling down through the facility type, e.g. if a facility is supplied by, 
+                        and reports to, a sub-county store, select the relevant sub-county store, then locate the
+                        facility name under it. 
+                        Go to <span style = 'color:green'>SITES ANALYTICS</span> to access
+                        list of facilities by program and by type. 
+                        Go to <span style = 'color:green'>ART REPORTS</span>to access reports limited to ART program,
+                        e.g. patient numbers.  Go to PROGRAMS to access the full list of created programs. 
+                        Go to <span style = 'color:green'> SUPPLY HIERARCHY </span> to access the supply chain hierarchy/site 
+                        classification by program.
+                    </p>
+                </div> ";
 
         echo "<div class = 'col-md-3' style = 'font-family:arial'>
-                <span style = 'color:blue;'>Updating the System</span>
+                <span style = 'color:blue;'>Updates</span>
                 <p class = 'lead text-justify' style = 'font-size:10pt'>
-                    There is need to query DHIS2 for newly created organization units and/or datasets.
-                    Use 
+                    The tool’s database requires updating for newly created organization units and/or datasets or edits of
+                    the existing data. This is done by aligning it with <a href = '".$dhis_url."' target = '_blank'> DHIS2 </a>.
+                    This menu is used to update the tool database from DHIS2 Web API.
+                    Go to 
                     <a  data-toggle='collapse' data-parent='#accordion' href='#collapseUpdates'>
                         UPDATES
                     </a> 
-                    to update the system database from DHIS2 Web API.
-                    Updates fetch new organization units and/or datasets that may have been created. 
-                    It also renames those that might have been renamed.
+                    and then select the relevant organisation unit to be updated, e.g.
+                    Counties, Sub counties, facilities, datasets.
+                </p>
+            </div> ";
 
+        echo "<div class = 'col-md-3' style = 'font-family:arial'>
+                <span style = 'color:blue;'>Administration</span>
+                <p class = 'lead text-justify' style = 'font-size:10pt'>
+                    This menu is used by Admin level users to modify the database, e.g. delete existing programs and
+                    their hierarchy, recover data deleted erroneously by other users.
                 </p>
             </div> ";
     }
